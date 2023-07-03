7b11b58606005d0889a95004fd79d0794b68fd45
TaekyungAn <atk0426@naver.com>
2023-07-03 22:28:18 +0900

    1.2 Actions

--- src/index.js
+++ src/index.js
@@ -6,8 +6,22 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 // reducer: data를 modify하는 함수
-const reducer = (state = 0) => {
-  return state;
+// action: reducer와 소통하기 위한 방법
+const reducer = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else return count;
 };
 const countStore = legacy_createStore(reducer);
+
+// dispatch: Reducer에게 Action을 보내는 방법
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
 console.log(countStore.getState());
