7a3b5173a5c1315dbd074da5c0fe7a04bcfcb0d1
TaekyungAn <atk0426@naver.com>
2023-07-03 22:36:43 +0900

    1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 // reducer: data를 modify하는 함수
 // action: reducer와 소통하기 위한 방법
 const reducer = (count = 0, action) => {
@@ -16,12 +18,20 @@ const reducer = (count = 0, action) => {
 };
 const countStore = legacy_createStore(reducer);
 
-// dispatch: Reducer에게 Action을 보내는 방법
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+// subscribe: store 안에 있는 변화 감지
+countStore.subscribe(onChange);
+
+// dispatch: Reducer에게 Action을 보내는 방법
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
