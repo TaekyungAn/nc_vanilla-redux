53a80c1a10367df2c39c1c0ff247b02663a28a58
TaekyungAn <atk0426@naver.com>
2023-07-05 23:13:57 +0900

    4.3 configureStore

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 import { combineReducers, legacy_createStore } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
@@ -6,30 +6,6 @@ import storage from "redux-persist/lib/storage";
 export const addToDo = createAction("ADD");
 export const deleteToDo = createAction("DELETE");
 console.log(addToDo, deleteToDo);
-
-/*const reducer = (state = [], action) => {
-  console.log(action);
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== parseInt(action.payload));
-    default:
-      return state;
-  }
-};*/
-
-//변경 전
-// toolkit으로 state mutate 가능
-/*const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
-  },
-  [deleteToDo]: (state, action) => {
-    // 강의에는 return이 없지만 그러면 작동 안됨. 그래서 추가하니 작동함
-    return state.filter((toDo) => toDo.id !== parseInt(action.payload));
-  },
-});*/
 
 // 경고: createReducer.ts:228 The object notation for `createReducer` is deprecated, and will be removed in RTK 2.0. Please use the 'builder callback' notation instead: https://redux-toolkit.js.org/api/createReducer
 // 변경 후
@@ -45,11 +21,18 @@ const reducer = createReducer([], (builder) => {
 
 const persistConfig = {
   key: "todo", //localStorage에 저장될 key값
-  storage: storage,
+  storage,
 };
-const allReducer = combineReducers({
+const rootReducer = combineReducers({
   reducer,
 });
-const store = legacy_createStore(persistReducer(persistConfig, allReducer));
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
 
 export default store;
